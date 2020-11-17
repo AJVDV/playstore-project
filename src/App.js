da7dd18ad23f197b7a2eb28592f0706279745554
Andrew VanDerVeen <69212799+AJVDV@users.noreply.github.com>
2020-11-16 16:09:02 -0800

    Creation and editing
    Did considerable testing and work locally only pushing now that project is roughly completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require('express');
+const morgan = require('morgan');
+const store = require('./playstore');
+const cors = require('cors');
+const app = express();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+app
+  .use(morgan('dev'))
+  .use(cors());
+
+
+app.get('/apps', (req, res) => {
+  const { sort, genres } = req.query;
+  let results = store;
+
+  if (sort) {
+    if (!['rating', 'app'].includes(sort)) {
+      return res
+        .status(400)
+        .send('sort must be one of rating or app');
+    }
+  }
+  if (sort === 'app') {
+    results = store.sort((a, b) => {
+      let x = a['App'].toLowerCase();
+      let y = b['App'].toLowerCase();
+
+      return x > y ? 1 : x < y ? -1 : 0;
+    });
+  }
+  else if (sort === 'rating') {
+    results = results.sort((a, b) => {
+      return a['Rating'] < b['Rating'] ? 1 : a['Rating'] > b['Rating'] ? -1 : 0;
+    })
+  }
+
+  if (genres) {
+    if (!['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'].includes(genres.toLowerCase())) {
+      return res.status(400).send('Genre must be one of Action, Puzzle, Strategy, Casual, Arcade or Card');
+    }
+    results = results.filter(app => {
+      return app.Genres.toLowerCase() === genres.toLowerCase();
+    });
+  }
+
+
+  return res.send(results);
+
+});
+module.exports = app;;
